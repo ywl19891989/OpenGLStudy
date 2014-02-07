51ea2061926c82d4b7d9423b06db2fd4729dfb90
ywl19891989 <hali.yang1989@gmail.com>
2014-02-07 22:02:27 +0800

    star

---
+++ star.mk
@@ -0,0 +1,18 @@
+
+LIBS:=-lopengl32 -lglu32 -lglut32 -lglut32win -lwinmm -gdi32
+
+Target:=star
+TargetDir:=./Debug
+OBJSDir:=$(TargetDir)/src
+SRCDir:=./src
+
+CXX:=g++
+
+$(Target) : $(Target).o
+	$(CXX) -g -o $(TargetDir)/$(Target).exe $(OBJSDir)/$(Target).o $(LIBS)
+
+$(Target).o : $(SRCDir)/$(Target).c
+	$(CXX) -c $(SRCDir)/$(Target).c -o $(OBJSDir)/$(Target).o
+		
+clean : 
+	rm $(TargetDir)/$(Target).exe $(OBJSDir)/$(Target).o
